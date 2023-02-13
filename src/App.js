46ac3bc443438d0b12a3931ffd5adc58a4222423
TharU0418 <101709936+TharU0418@users.noreply.github.com>
2023-02-13 22:46:38 +0530

    created ResultsPage1.js file and implemented it without any css
    created all the functions to get data from backend

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
-import TryItOut from "./pages/TryItOut";
+import ResultsPage1 from "./pages/ResultPage1";
+//import TryItOut from "./pages/TryItOut";
 
 function App() {
   return (
     <div className="App">
       App
 
-      <TryItOut />
+      {/* <TryItOut /> */}
+      <ResultsPage1 />
 
     </div>
   );
