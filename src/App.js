12037f96d68f17e8135b22356b0f5727409ac53c
MrDarkDrago <pasindusemage345@gmail.com>
2023-02-14 22:53:16 +0530

    tried to connect the nav bar to the app and failed

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
 import ResultsPage1 from "./pages/ResultPage1";
 //import TryItOut from "./pages/TryItOut";
+import Nav from './components/Nav';
 
 function App() {
   return (
+
     <div className="App">
+      <Nav />
       App
+      
 
       {/* <TryItOut /> */}
       <ResultsPage1 />
