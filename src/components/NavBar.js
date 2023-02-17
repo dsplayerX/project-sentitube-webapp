6728a4e86627a1b791c5811b47516f7c486208fe
MrDarkDrago <pasindusemage345@gmail.com>
2023-02-17 10:23:25 +0530

    Update NavBar.js
    added logo to the nav bar

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,12 +1,14 @@
 import React from "react";
 import { Link, useMatch, useResolvedPath } from "react-router-dom";
+import logo from './sentitube-icon.jpg';
 
 function NavBar() {
   return (
     <nav className="nav">
-      <Link to="/" className="site-title">
-        SentiTube
+      <Link to="/" className="site-logo">
+        <img src={logo} alt="Logo" className="logo" />
       </Link>
+      
       <ul>
         <li>
           <CustomLink to="/">Home</CustomLink>
