51cb5fd578ddc309fc9c815d5d33f77ac7267b65
MrDarkDrago <pasindusemage345@gmail.com>
2023-02-22 15:54:18 +0530

    updated the navbar

--- src/components/Navbarnew.js
+++ src/components/Navbarnew.js
@@ -2,23 +2,26 @@ import Container from 'react-bootstrap/Container';
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
 import { Link } from 'react-router-dom';
-import logo from './2ng.png';
+import logo from './Icons&Images/2ng.png';
 
 function Navbarnew() {
   return (
     <>
-      <Navbar bg="dark" variant="dark">
-        <Container>
-          <Navbar.Brand as={Link} to="/">
-            <img src={logo} alt="Logo" className="logo" />
-          </Navbar.Brand>
-          <Nav className="me-auto">
-            <Nav.Link as={Link} to="/TryItOut">TryItOut</Nav.Link>
-            <Nav.Link as={Link} to="/Contact">Contact</Nav.Link>
-            <Nav.Link as={Link} to="/About">About</Nav.Link>
-          </Nav>
-        </Container>
-      </Navbar>
+        <Navbar collapseOnSelect expand="lg" bg="light" variant="light">
+            <Container>
+                <Navbar.Brand as={Link} to="/">
+                    <img src={logo} alt="Logo" className="logo" />
+                </Navbar.Brand>
+                <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+                <Navbar.Collapse id="responsive-navbar-nav">
+                <Nav className="me-auto">
+                    <Nav.Link as={Link} to="/TryItOut">TryItOut</Nav.Link>
+                    <Nav.Link as={Link} to="/Contact">Contact</Nav.Link>
+                    <Nav.Link as={Link} to="/About">About</Nav.Link>
+                </Nav>
+                </Navbar.Collapse>
+            </Container>
+        </Navbar>
     </>
     
   );
