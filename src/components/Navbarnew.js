5bbbae38dee11c855cb2fd493ec1d7272bedaa05
MrDarkDrago <pasindusemage345@gmail.com>
2023-02-23 08:47:36 +0530

    Update Navbarnew.js

--- src/components/Navbarnew.js
+++ src/components/Navbarnew.js
@@ -15,7 +15,7 @@ function Navbarnew() {
                 <Navbar.Toggle aria-controls="responsive-navbar-nav" />
                 <Navbar.Collapse id="responsive-navbar-nav">
                 <Nav className="me-auto">
-                    <Nav.Link as={Link} to="/TryItOut">TryItOut</Nav.Link>
+                    <Nav.Link as={Link} to="/TryItOut" collapseOnSelect>TryItOut</Nav.Link>
                     <Nav.Link as={Link} to="/Contact">Contact</Nav.Link>
                     <Nav.Link as={Link} to="/About">About</Nav.Link>
                 </Nav>
