754686fdd86bef6f122092873571a4319ffa8e6b
dsplayerX <dumindu0043@gmail.com>
2023-02-23 10:50:14 +0530

    matched form content names with emailjs var names, added form component to the contact page

--- src/components/ContactUsForm.js
+++ src/components/ContactUsForm.js
@@ -1,7 +1,7 @@
 import React, { useRef } from "react";
 import emailjs from "@emailjs/browser";
 
-export const ContactUs = () => {
+export const ContactUsForm = () => {
   const form = useRef();
 
   const sendEmail = (e) => {
@@ -28,9 +28,9 @@ export const ContactUs = () => {
   return (
     <form ref={form} onSubmit={sendEmail}>
       <label>Name</label>
-      <input type="text" name="user_name" />
+      <input type="text" name="from_name" />
       <label>Email</label>
-      <input type="email" name="user_email" />
+      <input type="email" name="from_email" />
       <label>Message</label>
       <textarea name="message" />
       <input type="submit" value="Send" />
--- src/pages/Contact.js
+++ src/pages/Contact.js
@@ -1,11 +1,11 @@
 import React from "react";
-import { ContactUs } from "../components/ContactUs";
+import { ContactUsForm } from "../components/ContactUsForm";
 
 function Contact() {
   return (
     <div>
       <h1>Contact Us Page</h1>
-      {ContactUs}
+      <ContactUsForm />
     </div>
   );
 }
