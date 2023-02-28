cc5c279b26bc6cc4d264b4da3611092280be7f83
TharU0418 <101709936+TharU0418@users.noreply.github.com>
2023-02-28 19:07:37 +0530

    try to connect Result page and TryItOut page

--- src/pages/Results.js
+++ src/pages/Results.js
@@ -1,9 +1,23 @@
 import React from "react";
+import { useLocation } from 'react-router-dom';
 
 function Results() {
+
+  const location = useLocation
+  const results = location.state?.results;
+
   return (
     <div>
       <h1>Results</h1>
+      {results ? (
+        <ul>
+          {results.map((data) => (
+            <li key={data.id}>{data.name}</li>
+          ))}
+        </ul>
+      ) : (
+        <p>No results found</p>
+      )}
     </div>
   );
 }
