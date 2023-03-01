023d1c7e9eba74f0f092813da5d4cef4e083358f
Sanuri Perera <sanuriperera20@gmail.com>
2023-03-01 16:13:59 +0530

    Background Image for About us page
    Add the background image

--- src/pages/About.js
+++ src/pages/About.js
@@ -4,14 +4,24 @@ import { Link } from 'react-router-dom';
 
 
 
+
 function About() {
   return (
     <div>
-      <h1>About Page</h1>
+      <h1><center>About Us</center></h1>
+      <br/>
+
+      <p><center>If you want to learn about more about us, 
+                You are in the right place..... </center></p>
+
+      <h2><center>Who we are</center></h2>
+      <p><center>ajhgdHDKjdnkkjhiuguihjijHYTFDRE56ETYDB</center></p>
+
+      <h4>Our Mission</h4>
+      <h4>Our Vission</h4>
       <Link to="/MeetTheTeam">
         <button>Go to MeetTheTeam page</button>
       </Link>
-      
     </div>
   );
 }
