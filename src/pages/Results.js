fa10bd1cd3eb35a8d9bb273497759d6f3adcd5c3
TharU0418 <101709936+TharU0418@users.noreply.github.com>
2023-03-04 11:37:23 +0530

    design Result.js page

--- src/pages/Results.js
+++ src/pages/Results.js
@@ -1,15 +1,69 @@
 import React from "react";
-import { useLocation } from 'react-router-dom';
+//import { useLocation } from 'react-router-dom';
+import 'bootstrap/dist/css/bootstrap.min.css'
+
+import {Container, Row, Col} from 'react-bootstrap'
+
 
 function Results() {
 
-  const location = useLocation
-  const data = location.state;
+  //00const location = useLocation
+  //const data = location.state;
 
   return (
     <div>
-      <h1>Results</h1>
-      <p>{data}</p>
+      <h1>😁😐😩SentiTube😁😐😩</h1>
+      {/* <p>{data}</p> */}
+
+      
+
+      <Container
+        style={{
+          backgroundColor: 'rgb(51,51,255)',
+          color:'white'
+        }}
+      >
+        <h4>Final Result</h4>
+        <Row
+        style={{
+          backgroundColor: 'rgb(0,204,255)',
+          color:'black'
+        }}>
+          <Col>
+          
+            <p>Positve Comments</p>
+            <p>Neutral Comments</p>
+            <p>Negative Comments</p>
+            <p>Sarcastic Comments</p>
+            <p>Nonsarcastic Comments</p>
+            <p>Total Comments</p>
+          
+          </Col>
+        </Row>
+
+      </Container>
+        <br></br>
+      <Container
+        style={{
+          backgroundColor: 'rgb(51,51,255)',
+          color:'white'
+        }}
+      >
+        <h4>Custom Feedback</h4>
+        
+        <p>
+          Lorem Ipsum is simply dummy text of the printing and typesetting industry. 
+          Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, 
+          when an unknown printer took a galley of type and scrambled it to make a 
+          type specimen book. It has survived not only five centuries, but also the leap 
+          into electronic typesetting, remaining essentially unchanged. It was popularised
+           in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, 
+           and more recently with desktop publishing software like Aldus PageMaker including 
+           versions of Lorem Ipsum.
+        </p>
+
+      </Container>
+
     </div>
   );
 }
