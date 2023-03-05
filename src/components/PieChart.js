421aeb09cc880436c2da65c2a6a793bc26858b0e
dsplayerX <dumindu0043@gmail.com>
2023-03-05 12:00:59 +0530

    fixed show results and pie chart on results page

--- src/components/PieChart.js
+++ src/components/PieChart.js
@@ -1,14 +1,13 @@
-import React from 'react'
-import { Pie } from "react-chartjs-2"
-import { Chart as ChartJS } from "chart.js/auto";
-
+import React from "react";
+import { Pie } from "react-chartjs-2";
+//import { Chart as ChartJS } from "chart.js/auto";
 
 function PieChart({ chartData }) {
   return (
     <div>
-        <Pie data = {chartData} />;
+      <Pie data={chartData} options={{ responsive: true }} />
     </div>
-  )
+  );
 }
 
-export default PieChart
+export default PieChart;
--- src/pages/Results.js
+++ src/pages/Results.js
@@ -1,118 +1,102 @@
-import {useState} from "react";
-//import { useLocation } from 'react-router-dom';
-import 'bootstrap/dist/css/bootstrap.min.css'
-
-import {Container, Row, Col} from 'react-bootstrap'
-
+import { useLocation } from "react-router-dom";
+import "bootstrap/dist/css/bootstrap.min.css";
+import { Container, Row, Col } from "react-bootstrap";
 import PieChart from "../components/PieChart";
-import { UserData } from "./Data";
-import { UserData2 } from "./Data2";
 
 function Results() {
+  const location = useLocation();
+  const data = location.state;
+  //console.log("parsed data" + data);
 
-  //00const location = useLocation
-  //const data = location.state;
-
-  const [userData, setUserData] = useState({
-    labels: UserData.map((data) => data.type),
+  const sentimentChartData = {
+    labels: ["Positive", "Neutral", "Negative"],
     datasets: [
       {
-        label: "Users Gained",
-        data: UserData.map((data) => data.count),
-        backgroundColor: [
-          "#50AF95",
-          "#f3ba2f",
-          "#2a71d0",
+        label: "Sentiment Analysis",
+        data: [
+          data["Positive Comments"],
+          data["Neutral Comments"],
+          data["Negative Comments"],
         ],
-        borderColor: "black",
-        borderWidth: 2,
+        backgroundColor: ["#2ecc71", "#3498db", "#e74c3c"],
       },
     ],
-  });
+  };
 
-  const [userData2, setUserData2] = useState({
-    labels: UserData2.map((data) => data.type),
+  const sarcasmChartData = {
+    labels: ["Sarcastic", "Non-Sarcastic"],
     datasets: [
       {
-        label: "Users Gained",
-        data: UserData2.map((data) => data.count),
-        backgroundColor: [
-          "#50AF95",
-          "#f3ba2f",
-        ],
-        borderColor: "black",
-        borderWidth: 2,
+        label: "Sarcasm Analysis",
+        data: [data["Sarcastic Comments"], data["Nonsarcastic Comments"]],
+        backgroundColor: ["#f1c40f", "#9b59b6"],
       },
     ],
-  });
+  };
 
   return (
     <div className="App">
-      App
-      {/* <p>{data}</p> */}
-
+      <div>
+        <h4>Results</h4>
+      </div>
       <Container
         style={{
-          backgroundColor: 'rgb(204,204,255)',
-          color:'rgb(51,51,255)'
+          backgroundColor: "rgb(204,204,255)",
+          color: "rgb(51,51,255)",
         }}
       >
         <h4>Final Result</h4>
         <Row
-        style={{
-          backgroundColor: 'rgb(0,204,255)',
-          color:'black'
-        }}>
+          style={{
+            backgroundColor: "rgb(0,204,255)",
+            color: "black",
+          }}
+        >
           <Col>
-          
-            <p>Positve Comments</p>
-            <p>Neutral Comments</p>
-            <p>Negative Comments</p>
-            <p>Sarcastic Comments</p>
-            <p>Nonsarcastic Comments</p>
-            <p>Total Comments</p>
-          
+            <p>Positve Comments {data["Positive Comments"]}</p>
+            <p>Neutral Comments {data["Neutral Comments"]}</p>
+            <p>Negative Comments {data["Negative Comments"]}</p>
+            <p>Sarcastic Comments {data["Sarcastic Comments"]}</p>
+            <p>Non-Sarcastic Comments {data["Nonsarcastic Comments"]}</p>
+            <p>Total Comments {data["Total Comments"]}</p>
           </Col>
         </Row>
         <Row>
-
           <Col>
             <h5>Sentiment Result</h5>
             <div style={{ width: 260 }}>
-              <PieChart chartData={userData} />
+              <PieChart chartData={sarcasmChartData} />
             </div>
           </Col>
 
           <Col>
             <h5>Sarcasam Result</h5>
             <div style={{ width: 260 }}>
-              <PieChart chartData={userData2} />
+              <PieChart chartData={sentimentChartData} />
             </div>
           </Col>
-
         </Row>
-
       </Container>
       <br></br>
       <Container
         style={{
-          backgroundColor: 'rgb(51,51,255)',
-          color:'white'
+          backgroundColor: "rgb(51,51,255)",
+          color: "white",
         }}
       >
         <h4>Custom Feedback</h4>
-        
-        <p>
-          Lorem Ipsum is simply dummy text of the printing and typesetting industry. 
-          Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, 
-          when an unknown printer took a galley of type and scrambled it to make a 
-          type specimen book. It has survived not only five centuries, but also the leap 
-          into electronic typesetting, remaining essentially unchanged. It was popularised
-           in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, 
-           and more recently with desktop publishing software like Aldus PageMaker including 
-           versions of Lorem Ipsum.
-        </p>
 
+        <p>
+          Lorem Ipsum is simply dummy text of the printing and typesetting
+          industry. Lorem Ipsum has been the industry's standard dummy text ever
+          since the 1500s, when an unknown printer took a galley of type and
+          scrambled it to make a type specimen book. It has survived not only
+          five centuries, but also the leap into electronic typesetting,
+          remaining essentially unchanged. It was popularised in the 1960s with
+          the release of Letraset sheets containing Lorem Ipsum passages, and
+          more recently with desktop publishing software like Aldus PageMaker
+          including versions of Lorem Ipsum.
+        </p>
       </Container>
     </div>
   );
