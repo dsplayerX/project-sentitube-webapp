1a848fc0274c93adb000299f8daaa1e4d6794be3
TharU0418 <101709936+TharU0418@users.noreply.github.com>
2023-03-06 20:20:40 +0530

    restructured Result Page completely

--- src/pages/Results.js
+++ src/pages/Results.js
@@ -12,6 +12,20 @@ function Results() {
   //console.log("parsed data" + data);
   const navigate = useNavigate();
 
+  const finalsentiChartData = {
+    labels: ["Positive", "Negative"],
+    datasets: [
+      {
+        label: "Final Sentiment Analysis",
+        data: [
+          data["Sentitube Positve"],
+          data["Sentitube Negative"],
+        ],
+        backgroundColor: ["#009900", "#E92611"],
+      },
+    ],
+  };
+
   const sentimentChartData = {
     labels: ["Positive", "Neutral", "Negative"],
     datasets: [
@@ -58,53 +72,27 @@ function Results() {
       <Container
         style={{
           color: "rgb(51,51,255)",
+          marginBottom:"20px"
         }}
       >
-        <h4>Final Result</h4>
-        <Row
-          style={{
-            background:
-              "linear-gradient(45deg, rgb(254, 0, 0), rgb(255, 255, 100))",
-            color: "white",
-            padding: "10px",
-            margin: "6px",
+        
+        <Row 
+          style={{ 
+            textAlign: "center",
+            justifyContent: "center",
+            marginBottom:"10px"
           }}
         >
-          <Col
-            style={{
-              fontFamily: "'Dosis', sans-serif",
-              fontSize: "20px",
-            }}
-          >
-            <p>Positve Comments : {data["Positive Comments"]}</p>
-            <p>Neutral Comments : {data["Neutral Comments"]}</p>
-            <p>Negative Comments : {data["Negative Comments"]}</p>
-            <p>Sarcastic Comments : {data["Sarcastic Comments"]}</p>
-            <p>Non-Sarcastic Comments : {data["Nonsarcastic Comments"]}</p>
-            <p className="total">Total Comments -- {data["Total Comments"]}</p>
-          </Col>
-          <Col>
-            <Button
-              varient="flat"
-              size="xl"
-              style={{
-                backgroundColor: "white",
-                border: "4px",
-                color: "red",
-                borderRadius: "20px",
-                borderColor: "black",
-              }}
-              onClick={handleButton}
-            >
-              Per Comment Results
-            </Button>
-          </Col>
+            <h5 className="chart-title1">Final Sentiment Result</h5>
+            <div style={{ width: 340 }}>
+              <PieChart chartData={finalsentiChartData} />
+            </div>
         </Row>
         <Row>
           <Col>
             <h5 className="chart-title">Sentiment Result</h5>
             <div style={{ width: 260 }}>
-              <PieChart chartData={sentimentChartData} />
+              <PieChart chartData={sentimentChartData}/>
             </div>
           </Col>
 
@@ -116,7 +104,7 @@ function Results() {
           </Col>
         </Row>
       </Container>
-      <br></br>
+      
       <Container
         style={{
           color: "black",
@@ -125,7 +113,81 @@ function Results() {
       >
         <h4>Custom Feedback</h4>
 
-        <p>{feedbackItem.text}</p>
+        <p className="feedback-box">{feedbackItem.text}</p>
+
+        <h4>Final Result</h4>
+        <Row
+          style={{
+            background:
+              "linear-gradient(45deg, rgb(254, 0, 0), rgb(255, 201, 100))",
+            color: "white",
+            padding: "10px",
+            margin: "6px",
+          }}
+        >
+          <Row>
+          <Col
+            style={{
+              fontFamily: "'Dosis', sans-serif",
+              fontSize: "20px",
+              textAlign:"left"
+            }}
+          >
+            <h6>Final Senti Result</h6>
+            <p>Senti Positve Comments : {data["Sentitube Positve"]}</p>
+            <p>Senti Negative Comments : {data["Sentitube Negative"]}</p>
+          </Col>
+          <Col
+          style={{
+            fontFamily: "'Dosis', sans-serif",
+            fontSize: "20px",
+            textAlign:"left"
+          }}
+          >
+            <h6>Sentiment Result</h6>
+            <p>Positve Comments : {data["Positive Comments"]}</p>
+            <p>Neutral Comments : {data["Neutral Comments"]}</p>
+            <p>Negative Comments : {data["Negative Comments"]}</p></Col>
+          <Col
+          style={{
+            fontFamily: "'Dosis', sans-serif",
+            fontSize: "20px",
+            textAlign:"left"
+          }}
+          >
+            <h6>Sarcasam Result</h6>
+            <p>Sarcastic Comments : {data["Sarcastic Comments"]}</p>
+            <p>Non-Sarcastic Comments : {data["Nonsarcastic Comments"]}</p></Col>
+          </Row>
+          <Row
+          style={{
+            marginTop:"20px"
+          }}
+          >                   
+            <Col>
+              <Button
+                varient="flat"
+                size="xl"
+                style={{
+                  backgroundColor: "rgb(255,255,102)",
+                  border: "4px",
+                  color: "red",
+                  borderRadius: "20px",
+                  borderColor: "red",
+                }}
+                onClick={handleButton}
+              >
+                Per Comment Results
+              </Button>
+            </Col>
+            <Col>
+          <p className="total">Total Comments -- {data["Total Comments"]}</p>
+
+          </Col>
+          </Row>
+          
+        </Row>
+
       </Container>
     </div>
   );
