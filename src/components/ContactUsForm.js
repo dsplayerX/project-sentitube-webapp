c6b5bbcf2f525a1dcadd75902e63cabc84c89126
TharU0418 <101709936+TharU0418@users.noreply.github.com>
2023-03-06 23:31:33 +0530

    Added Card element in to Contact.js page

--- src/components/ContactUsForm.js
+++ src/components/ContactUsForm.js
@@ -75,84 +75,71 @@ export const ContactUsForm = () => {
 
   return (
     <>
-      <Container>
-        <Row>
-          <Col>
-            <Card className='nb-3' style={{color:"red"}}>
-              <Card.Img  src="https://www.google.com/imgres?imgurl=https%3A%2F%2Fcdn.searchenginejournal.com%2Fwp-content%2Fuploads%2F2022%2F06%2Fimage-search-1600-x-840-px-62c6dc4ff1eee-sej.png&imgrefurl=https%3A%2F%2Fwww.searchenginejournal.com%2Fbest-image-search-engines%2F299963%2F&tbnid=Pso-9ayGpy0KlM&vet=12ahUKEwiO667qn8D9AhW7hGMGHeecBC8QMygEegUIARDCAQ..i&docid=faQEvyb6ULMOUM&w=1600&h=840&itg=1&q=image&ved=2ahUKEwiO667qn8D9AhW7hGMGHeecBC8QMygEegUIARDCAQ"/>
-              <Card.Body>
-                <Card.Title>Card Exsample</Card.Title>
-              <Card.Text>
-                bootstrap
-              </Card.Text>
-              <Button varient='primary'>Read More</Button>
-              </Card.Body>
-            </Card>
-          </Col>
-          <Col>
-            <Form ref={form} onSubmit={sendEmail}>
-              <Form.Group className="mb-3" controlId="exampleForm.ControlInput1">
-                <Form.Label>Name</Form.Label>
-                <Form.Control
-                  type="text"
-                  placeholder="Your name"
-                  name="from_name"
-                  isInvalid={!!formErrors.name}
-                />
-                {/* showing errors below the form input field */}
-                {formErrors.name && (
-                  <Form.Control.Feedback type="invalid">
-                    {formErrors.name}
-                  </Form.Control.Feedback>
-                )}
-              </Form.Group>
-              <Form.Group className="mb-3" controlId="exampleForm.ControlInput1">
-                <Form.Label>Email address</Form.Label>
-                <Form.Control
-                  type="email"
-                  placeholder="name@example.com"
-                  name="from_email"
-                  isInvalid={!!formErrors.email}
-                />
-                {formErrors.email && (
-                  <Form.Control.Feedback type="invalid">
-                    {formErrors.email}
-                  </Form.Control.Feedback>
-                )}
-              </Form.Group>
-              <Form.Group className="mb-3" controlId="exampleForm.ControlTextarea1">
-                <Form.Label>Message</Form.Label>
-                <Form.Control
-                  as="textarea"
-                  name="message"
-                  rows={3}
-                  isInvalid={!!formErrors.message}
-                />
-                {formErrors.message && (
-                  <Form.Control.Feedback type="invalid">
-                    {formErrors.message}
-                  </Form.Control.Feedback>
-                )}
-              </Form.Group>
-              <Button className="submit-button" value="Send" type="submit">
-                Send
-              </Button>
-            </Form>
+      <Form ref={form} onSubmit={sendEmail}
+        style={{
+          color:"black"
+        }}
+      >
+        <Form.Group className="mb-3" controlId="exampleForm.ControlInput1">
+          <Form.Label>Name</Form.Label>
+          <Form.Control
+            type="text"
+            placeholder="Your name"
+            name="from_name"
+            isInvalid={!!formErrors.name}
+          />
+          {/* showing errors below the form input field */}
+          {formErrors.name && (
+            <Form.Control.Feedback type="invalid">
+              {formErrors.name}
+            </Form.Control.Feedback>
+          )}
+        </Form.Group>
+        <Form.Group className="mb-3" controlId="exampleForm.ControlInput1">
+          <Form.Label>Email address</Form.Label>
+            <Form.Control
+              type="email"
+              placeholder="name@example.com"
+              name="from_email"
+              isInvalid={!!formErrors.email}
+            />
+            {formErrors.email && (
+              <Form.Control.Feedback type="invalid">
+                {formErrors.email}
+              </Form.Control.Feedback>
+            )}
+        </Form.Group>
+        <Form.Group className="mb-3" controlId="exampleForm.ControlTextarea1">
+          <Form.Label>Message</Form.Label>
+          <Form.Control
+            as="textarea"
+            name="message"
+            rows={3}
+            isInvalid={!!formErrors.message}
+          />
+          {formErrors.message && (
+            <Form.Control.Feedback type="invalid">
+              {formErrors.message}
+            </Form.Control.Feedback>
+          )}
+        </Form.Group>
+        <Button className="submit-button" value="Send" type="submit">
+          Send
+        </Button>
+        </Form>
 
-            <Modal show={showModal} onHide={handleCloseModal}>
-              <Modal.Header closeButton>
-                <Modal.Title>Message Sent</Modal.Title>
-              </Modal.Header>
-              <Modal.Body>Your message has been sent successfully.</Modal.Body>
-              <Modal.Footer>
-                <Button variant="secondary" onClick={handleCloseModal}>
-                  Close
-                </Button>
-              </Modal.Footer>
-            </Modal>
-          </Col>
-        </Row>
-      </Container>
+        <Modal show={showModal} onHide={handleCloseModal}>
+          <Modal.Header closeButton>
+            <Modal.Title>Message Sent</Modal.Title>
+           </Modal.Header>
+          <Modal.Body>Your message has been sent successfully.</Modal.Body>
+            <Modal.Footer>
+              <Button variant="secondary" onClick={handleCloseModal}>
+                Close
+              </Button>
+           </Modal.Footer>
+        </Modal>
+
     </>
   );
 };
--- src/pages/Contact.js
+++ src/pages/Contact.js
@@ -1,11 +1,57 @@
 import React from "react";
 import { ContactUsForm } from "../components/ContactUsForm";
+import { Form, Button, Modal, Container, Row, Col, Card } from "react-bootstrap";
+import 'bootstrap/dist/css/bootstrap.min.css'
 
 function Contact() {
   return (
     <div>
-      <h1>Contact Us Page</h1>
-      <ContactUsForm />
+      
+      <Container
+        style={{
+          marginTop:"10px"
+        }}
+      >
+        <Row>
+          <Col>
+          <Card className='nb-3'
+            style={{
+              backgroundColor:"rgb(51,204,51)",
+              border:"2px green solid",
+              borderRadius:"4px",
+              width:"260px",
+              marginTop:"80px"
+             }}
+          >
+              <Card.Body 
+                
+              
+              >
+                <Card.Title
+                 style={{
+                  fontSize:"20px",
+                  color:"black"
+                 }}
+                >
+                  📧 - Email Us</Card.Title>
+              <Card.Text
+               style={{
+                fontSize:"18px",
+                color:"black"
+               }}
+              >
+                sentitube@gmail.com
+              </Card.Text>
+              </Card.Body>
+            </Card>
+          </Col>
+          <Col>
+          <ContactUsForm />
+          </Col>
+        </Row>
+      </Container>
+
+      
     </div>
   );
 }
