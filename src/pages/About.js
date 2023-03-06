ea4c3cdc1c9c990e51a24e260400ec0b7d443327
dsplayerX <dumindu0043@gmail.com>
2023-03-06 20:04:30 +0530

    added number of results to analyse dropdown, used bootstrap components in tryitout, installed url-parse module

--- src/pages/About.js
+++ src/pages/About.js
@@ -1,37 +1,37 @@
 import React from "react";
-import { Link } from 'react-router-dom';
+import { Link } from "react-router-dom";
 import "./About.css";
-import about_bgimage from "./about_bgimage.jpg";
-
-
-
+import about_bgimage from "../images/about_bgimage.jpg";
 
 function About() {
   return (
     <div>
-
       <div class="bg-image"></div>
-      <img src={about_bgimage}   />
-        <div class="bg-text">
-          <h1>About Us</h1>
-          <p>If you want to learn about more about us, 
-                You are in the right place.....</p>
-        </div>
+      <img src={about_bgimage} />
+      <div class="bg-text">
+        <h1>About Us</h1>
+        <p>
+          If you want to learn about more about us, You are in the right
+          place.....
+        </p>
+      </div>
 
-        <div class ="container">
-          <div class="centered">
-          <h2><center>Who we are</center></h2>
-          <p><center></center></p>
+      <div class="container">
+        <div class="centered">
+          <h2>
+            <center>Who we are</center>
+          </h2>
+          <p>
+            <center></center>
+          </p>
           <h4>Our Mission</h4>
           <h4>Our Vission</h4>
-          
+
           <Link to="/MeetTheTeam">
             <button>Go to MeetTheTeam page</button>
           </Link>
-          </div>
-
         </div>
-        
+      </div>
     </div>
   );
 }
--- src/pages/TryItOut.js
+++ src/pages/TryItOut.js
@@ -1,37 +1,43 @@
 import React, { useState } from "react";
 import { useNavigate } from "react-router-dom";
 import axios from "axios";
-import Results from "./Results";
+import { Form, Button, Container } from "react-bootstrap";
+import { Dropdown, DropdownButton } from "react-bootstrap";
+import { FormGroup, FormLabel, FormControl, Row, Col } from "react-bootstrap";
 
 function TryItOut() {
   const [inputValue, setInputValue] = useState("");
+  const [numResults, setNumResults] = useState(250); // default value is 100
   const navigate = useNavigate();
 
-  const[responseData, setResponseData] = useState(null);
+  const [isAnalysing, setIsAnalysing] = useState(false);
 
   const handleButtonClick = async (event) => {
     event.preventDefault();
     try {
+      // Set isAnalysing to true
+      setIsAnalysing(true);
       const response = await axios
         .post("http://localhost:5000/analysisresults", {
           userinput: inputValue,
+          numresults: numResults,
         })
         .then((response) => {
           // Handle the response from the server
           console.log(response.data);
           navigate("/Results", { state: response.data });
-
         })
         .catch((error) => {
           // Handle errors that occur during the request
           console.error(error);
         });
-        
-        console.log(response.data);
+
+      console.log(response.data);
       // request succeeded, navigate to results page
       ///setResponseData(response.data);
     } catch (error) {
       // request failed, handle error
+      console.error(error);
     }
   };
 
@@ -40,23 +46,66 @@ function TryItOut() {
     setInputValue(event.target.value);
   };
 
-  return (
-    <div className="tryitoutmain">
-      <div>
-        <h1>Try It Out Page</h1>
-      </div>
-      {/* input search bar */}
-      <input
-        type="text"
-        placeholder="enter youtube link"
-        value={inputValue} //the value passed
-        onChange={handleInputChange}
-      />
-      <button onClick={handleButtonClick}>🔍 search</button>
-    
+  const handleNumResultsSelect = (num) => {
+    // Update the number of results when the user selects a value from the dropdown
+    setNumResults(num);
+  };
 
-    </div>
+  return (
+    <Container className="tryitoutmain" style={{ color: "black" }}>
+      <Form>
+        <Form.Group controlId="formYoutubeLink">
+          <Form.Label>YouTube Link</Form.Label>
+          <Form.Control
+            type="text"
+            placeholder="Enter YouTube link"
+            value={inputValue}
+            onChange={handleInputChange}
+          />
+        </Form.Group>
+
+        <Form.Group controlId="formNumResults">
+          <Col>
+            <Row>
+              <Form.Label>Number of Results</Form.Label>
+            </Row>
+            <Row>
+              <DropdownButton
+                id="num-results-dropdown"
+                title={`${numResults}`}
+                variant="secondary"
+              >
+                <Dropdown.Item onClick={() => handleNumResultsSelect(100)}>
+                  100
+                </Dropdown.Item>
+                <Dropdown.Item onClick={() => handleNumResultsSelect(250)}>
+                  250
+                </Dropdown.Item>
+                <Dropdown.Item onClick={() => handleNumResultsSelect(500)}>
+                  500
+                </Dropdown.Item>
+                <Dropdown.Item onClick={() => handleNumResultsSelect(1000)}>
+                  1000
+                </Dropdown.Item>
+                <Dropdown.Item onClick={() => handleNumResultsSelect(5000)}>
+                  5000
+                </Dropdown.Item>
+              </DropdownButton>
+            </Row>
+          </Col>
+        </Form.Group>
+
+        <Form.Group>
+          <Button
+            variant="primary"
+            onClick={handleButtonClick}
+            disabled={isAnalysing}
+          >
+            {isAnalysing ? "Analyzing..." : "Analyze"}
+          </Button>
+        </Form.Group>
+      </Form>
+    </Container>
   );
 }
-
 export default TryItOut;
