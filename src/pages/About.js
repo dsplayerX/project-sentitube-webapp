ab0980d6838089d74e869892124748e331366874
Sanuri Perera <sanuriperera20@gmail.com>
2023-03-09 17:58:11 +0530

    Remove background image on about us page

--- src/pages/About.js
+++ src/pages/About.js
@@ -1,37 +1,26 @@
 import React from "react";
 import { Link } from 'react-router-dom';
-import "./About.css";
-import about_bgimage from "./about_bgimage.jpg";
-
-
-
+import "../CSS/about.css";
 
 function About() {
   return (
     <div>
-
-      <div class="bg-image"></div>
-      <img src={about_bgimage}   />
-        <div class="bg-text">
-          <h1>About Us</h1>
-          <p>If you want to learn about more about us, 
-                You are in the right place.....</p>
+      <h1><center>Vision & Mission</center></h1>
+      <div class ="row">
+        <div class="column">
+          <h2>Our Vision</h2>
+          <p>text ............</p>
         </div>
 
-        <div class ="container">
-          <div class="centered">
-          <h2><center>Who we are</center></h2>
-          <p><center></center></p>
-          <h4>Our Mission</h4>
-          <h4>Our Vission</h4>
-          
-          <Link to="/MeetTheTeam">
-            <button>Go to MeetTheTeam page</button>
-          </Link>
-          </div>
-
+        <div class ="column">
+          <h2>Our Mission</h2>
+          <p>text ..............</p>
         </div>
-        
+      </div>
+
+      <Link to="/MeetTheTeam">
+        <button>Go to MeetTheTeam page</button>
+      </Link>
     </div>
   );
 }
