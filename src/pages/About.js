a4e56d6dfdff6a031035880dca26349649b57934
Sanuri Perera <sanuriperera20@gmail.com>
2023-03-11 11:05:48 +0530

    Added our mission into about us page

--- src/pages/About.js
+++ src/pages/About.js
@@ -5,17 +5,21 @@ import "../CSS/about.css";
 function About() {
   return (
     <div>
-      <h1><center>Vision & Mission</center></h1>
+      <h1><center><b>Vision & Mission</b></center></h1>
       <div class ="row">
         <div class="column">
-          <h1>Our Vision</h1>
-          <p>To provide accurate sentiment results and personalized commentary 
+          <h3 class ="thick">Our Vision</h3>
+          <p align ="left">To provide accurate sentiment results and personalized commentary 
             for YouTube videos by combining sentiment and sarcasm analysis.</p>
         </div>
 
         <div class ="column">
-          <h2>Our Mission</h2>
-          <p>text ..............</p>
+          <h3 class ="thick">Our Mission</h3>
+          <p align ="left">To create, put into practice, and assess a solution that will make 
+            it simple and quick for YouTube users to locate worthwhile content. 
+            To recognize both sentiments and sarcasm, it will use sentiment and 
+            sarcasm analysis,which will increase the detection system's accuracy 
+            and performance rate.</p>
         </div>
       </div>
 
