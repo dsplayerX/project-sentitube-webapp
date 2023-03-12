7577520b594ac9a34a85276e0113c7c4a239dbf5
Hasithjay98 <eshan.20210150@iit.ac.lk>
2023-03-13 00:42:57 +0530

    changed MeetTheTeam page

--- src/pages/MeetTheTeam.js
+++ src/pages/MeetTheTeam.js
@@ -1,94 +1,79 @@
 import React, { Component } from "react";
+import Card from 'react-bootstrap/Card';
+import CardGroup from 'react-bootstrap/CardGroup';
 
 function MeetTheTeam() {
   return (
-    <div>
-      <h1 className="teambcg">Meet The Team</h1>
-      <div className="member1">
-        <div>
-          <div className="Member1upper-container">
-            <div className="Member1image-container">
-              <img src="" alt="" height="100px" width="100px" />
-            </div>
-          </div>
-        </div>
-        <div className="Member1lower-container">
-          <h2 className="member1-title">Dumindu Sameendra</h2>
-          <p className="member1-text"></p>
-          <a href="#" className="member1-link">
-            Card Link
-          </a>
-        </div>
-      </div>
-
-      <div className="member2">
-        <div>
-          <div className="Member2upper-container">
-            <div className="Member2image-container">
-              <img src="" height="100px" width="100px" />
-            </div>
-          </div>
-        </div>
-        <div className="Member2lower-container">
-          <h2 className="member2-title">Tharusha Dilshan</h2>
-          <p className="member2-text"></p>
-          <a href="#" className="member1-link">
-            Card Link
-          </a>
-        </div>
-      </div>
-
-      <div className="member3">
-        <div>
-          <div className="Member3upper-container">
-            <div className="Member3image-container">
-              <img src="" alt="" height="100px" width="100px" />
-            </div>
-          </div>
-        </div>
-        <div className="Member3lower-container">
-          <h2 className="member3-title">Pasindu De Silva</h2>
-          <p className="member3-text"></p>
-          <a href="#" className="member1-link">
-            Card Link
-          </a>
-        </div>
-      </div>
-
-      <div className="member4">
-        <div>
-          <div className="Member4upper-container">
-            <div className="Member4image-container">
-              <img src="" alt="" height="100px" width="100px" />
-            </div>
-          </div>
-        </div>
-        <div className="Member4lower-container">
-          <h2 className="member4-title">Eshan Hasitha</h2>
-          <p className="member4-text"></p>
-          <a href="#" className="member1-link">
-            Card Link
-          </a>
-        </div>
-      </div>
-
-      <div className="member5">
-        <div>
-          <div className="Member5upper-container">
-            <div className="Member5image-container">
-              <img src="" alt="" height="100px" width="100px" />
-            </div>
-          </div>
-        </div>
-        <div className="Member5lower-container">
-          <h2 className="member5-title">Sanuri</h2>
-          <p className="member5-text"></p>
-          <a href="#" className="member1-link">
-            Card Link
-          </a>
-        </div>
-      </div>
-    </div>
+    <CardGroup>
+      <Card>
+        <Card.Img variant="top" src="https://images.pexels.com/photos/837358/pexels-photo-837358.jpeg?auto=compress&cs=tinysrgb&w=1600" />
+        <Card.Body>
+          <Card.Title>Eshan Hasitha</Card.Title>
+          <Card.Text>
+            This is a wider card with supporting text below as a natural lead-in
+            to additional content. This content is a little bit longer.
+          </Card.Text>
+        </Card.Body>
+        <Card.Footer>
+          <small className="text-muted">Last updated 3 mins ago</small>
+        </Card.Footer>
+      </Card>
+      <Card>
+        <Card.Img variant="top" src="https://images.pexels.com/photos/837358/pexels-photo-837358.jpeg?auto=compress&cs=tinysrgb&w=1600" />
+        <Card.Body>
+          <Card.Title>Card title</Card.Title>
+          <Card.Text>
+            This card has supporting text below as a natural lead-in to
+            additional content.{' '}
+          </Card.Text>
+        </Card.Body>
+        <Card.Footer>
+          <small className="text-muted">Last updated 3 mins ago</small>
+        </Card.Footer>
+      </Card>
+      <Card>
+        <Card.Img variant="top" src="https://images.pexels.com/photos/837358/pexels-photo-837358.jpeg?auto=compress&cs=tinysrgb&w=1600" />
+        <Card.Body>
+          <Card.Title>Card title</Card.Title>
+          <Card.Text>
+            This is a wider card with supporting text below as a natural lead-in
+            to additional content. This card has even longer content than the
+            first to show that equal height action.
+          </Card.Text>
+        </Card.Body>
+        <Card.Footer>
+          <small className="text-muted">Last updated 3 mins ago</small>
+        </Card.Footer>
+      </Card>
+      <Card>
+        <Card.Img variant="top" src="https://images.pexels.com/photos/837358/pexels-photo-837358.jpeg?auto=compress&cs=tinysrgb&w=1600" />
+        <Card.Body>
+          <Card.Title>Card title</Card.Title>
+          <Card.Text>
+            This is a wider card with supporting text below as a natural lead-in
+            to additional content. This card has even longer content than the
+            first to show that equal height action.
+          </Card.Text>
+        </Card.Body>
+        <Card.Footer>
+          <small className="text-muted">Last updated 3 mins ago</small>
+        </Card.Footer>
+      </Card>
+      <Card>
+        <Card.Img variant="top" src="https://images.pexels.com/photos/837358/pexels-photo-837358.jpeg?auto=compress&cs=tinysrgb&w=1600" />
+        <Card.Body>
+          <Card.Title>Card title</Card.Title>
+          <Card.Text>
+            This is a wider card with supporting text below as a natural lead-in
+            to additional content. This card has even longer content than the
+            first to show that equal height action.
+          </Card.Text>
+        </Card.Body>
+        <Card.Footer>
+          <small className="text-muted">Last updated 3 mins ago</small>
+        </Card.Footer>
+      </Card>
+    </CardGroup>
   );
 }
 
