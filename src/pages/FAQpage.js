57e78e85b5dee2ac8089079d8bddfdddff46706a
TharU0418 <101709936+TharU0418@users.noreply.github.com>
2023-03-13 17:44:06 +0530

    Add a function into FAQ page

--- src/pages/FAQpage.js
+++ src/pages/FAQpage.js
@@ -1,70 +1,44 @@
-import React from 'react'
+import React, {useState} from 'react'
 import { Container, Row, Col, Button } from "react-bootstrap";
 import 'bootstrap/dist/css/bootstrap.min.css'
 import 'bootstrap-icons/font/bootstrap-icons.css';
+import { MDBAccordion, MDBAccordionItem, MDBContainer } from "mdb-react-ui-kit";
 
 
 function FAQpage() {
+
+  const [isvisible, setVisible] = useState(true);
+
+  const handleClick = () => {
+    setVisible(!isvisible);
+  }
+
   return (
-    <div className='faq-page'>
-      <h2>Frequently Asked Questions</h2>
-
-      <Container style={{
-        margin:"auto",
-        textAlign:"center",
-        justifyContent:"center"
+    <MDBContainer className="mt-5" style={{
+      maxWidth: '1000px',
+      backgroundColor:'#f6bdbd',
+      margin:"20px auto 20px"
+      
       }}>
-        <Row>
-          <div className='faq-box'>
-            <h6>Lorem Ipsom Lorem Ipsom Lorem Ipsom Lorem Ipsom</h6>
-            <i class="bi bi-plus" style={{ 
-              fontSize: 20,
-              margin:"auto",
-              display:"flex",
-              textAlign:"right",
-              justifyContent:"right" 
-            }}></i>
-          </div>
-        </Row>
-        <Row>
-          <div className='faq-box'>
-              <h6>Lorem Ipsom Lorem Ipsom Lorem Ipsom Lorem Ipsom</h6>
-              <i class="bi bi-plus" style={{ 
-                fontSize: 20,
-                margin:"auto",
-                display:"flex",
-                textAlign:"right",
-                justifyContent:"right" 
-              }}></i>
-            </div>
-        </Row>
-        <Row>
-          <div className='faq-box'>
-            <h6>Lorem Ipsom Lorem Ipsom Lorem Ipsom Lorem Ipsom</h6>
-            <i class="bi bi-plus" style={{ 
-              fontSize: 20,
-              margin:"auto",
-              display:"flex",
-              textAlign:"right",
-              justifyContent:"right" 
-            }}></i>
-          </div>
-        </Row>
-        <Row>
-          <div className='faq-box'>
-              <h6>Lorem Ipsom Lorem Ipsom Lorem Ipsom Lorem Ipsom</h6>
-              <i class="bi bi-plus" style={{ 
-                fontSize: 20,
-                margin:"auto",
-                display:"flex",
-                textAlign:"right",
-                justifyContent:"right" 
-              }}></i>
-            </div>
-        </Row>
-      </Container>
+      <MDBAccordion>
+        <MDBAccordionItem collapseId={1} headerTitle="Can I use this site to get a feedback about a facebook video" >
+          No, You can't. Only for YouTube videoes.
+        </MDBAccordionItem>
+        <MDBAccordionItem collapseId={2} headerTitle="Can I use this site to get a feedback about a facebook video">
+          No, You can't. Only for YouTube videoes.
+        </MDBAccordionItem>
+        <MDBAccordionItem collapseId={3} headerTitle="Can I use this site to get a feedback about a facebook video">
+          No, You can't. Only for YouTube videoes.
+        </MDBAccordionItem>
+        <MDBAccordionItem collapseId={4} headerTitle="Can I use this site to get a feedback about a facebook video">
+          No, You can't. Only for YouTube videoes.
+        </MDBAccordionItem>
+        <MDBAccordionItem collapseId={5} headerTitle="Can I use this site to get a feedback about a facebook video">
+          No, You can't. Only for YouTube videoes.
+        </MDBAccordionItem>
 
-    </div>
+      </MDBAccordion>
+    </MDBContainer>
   )
 }
 
