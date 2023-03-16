6039b07c53cdd0bb173123509f11adfef9b77ad2
dsplayerX <dumindu0043@gmail.com>
2023-03-16 14:38:42 +0530

    added press enter to analyse, changed some links to different pages, changed no of comments  dropdown

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,38 +1,57 @@
-import { faLayerGroup } from '@fortawesome/free-solid-svg-icons';
-import 'bootstrap/dist/css/bootstrap.min.css';  
-import { Navbar, Container, Nav, NavDropdown, Offcanvas, Form, FormControl,Button } from 'react-bootstrap'; 
-import { Link } from 'react-router-dom';
-import logo from '../images/senti_logo.png';
+import { faLayerGroup } from "@fortawesome/free-solid-svg-icons";
+import "bootstrap/dist/css/bootstrap.min.css";
+import {
+  Navbar,
+  Container,
+  Nav,
+  NavDropdown,
+  Offcanvas,
+  Form,
+  FormControl,
+  Button,
+} from "react-bootstrap";
+import { Link } from "react-router-dom";
+import logo from "../images/senti_logo.png";
 
 function NavBar() {
   return (
-      <>  
-        <Navbar bg="light" expand="lg">  
-        <Container fluid>  
-            <Navbar.Brand as={Link} to="/">
-                <img src={logo} alt="Logo" className="logo" />
-            </Navbar.Brand>
-            <Navbar.Toggle aria-controls="offcanvasNavbar" />  
-            <Navbar.Offcanvas  
-                id="offcanvasNavbar"  
-                aria-labelledby="offcanvasNavbarLabel"  
-                placement="end"  
-            >  
-            <Offcanvas.Header closeButton>  
-                <Offcanvas.Title id="offcanvasNavbarLabel">Sidebar</Offcanvas.Title>  
-            </Offcanvas.Header>  
-            <Offcanvas.Body>  
-                <Nav className="justify-content-end flex-grow-1 pe-3">  
-                <Nav.Link as={Link} to="/">Home</Nav.Link>
-                    <Nav.Link as={Link} to="/TryItOut">TryItOut</Nav.Link>
-                    <Nav.Link as={Link} to="/Contact">Contact</Nav.Link>
-                    <Nav.Link as={Link} to="/About">About</Nav.Link>    
-                </Nav>  
-            </Offcanvas.Body>  
-            </Navbar.Offcanvas>  
-        </Container>  
-        </Navbar>  
-    </>  
+    <>
+      <Navbar bg="light" expand="lg">
+        <Container fluid>
+          <Navbar.Brand as={Link} to="/">
+            <img src={logo} alt="Logo" className="logo" />
+          </Navbar.Brand>
+          <Navbar.Toggle aria-controls="offcanvasNavbar" />
+          <Navbar.Offcanvas
+            id="offcanvasNavbar"
+            aria-labelledby="offcanvasNavbarLabel"
+            placement="end"
+          >
+            <Offcanvas.Header closeButton>
+              <Offcanvas.Title id="offcanvasNavbarLabel">
+                Sidebar
+              </Offcanvas.Title>
+            </Offcanvas.Header>
+            <Offcanvas.Body>
+              <Nav className="justify-content-end flex-grow-1 pe-3">
+                <Nav.Link as={Link} to="/">
+                  Home
+                </Nav.Link>
+                <Nav.Link as={Link} to="/TryItOut">
+                  TryItOut
+                </Nav.Link>
+                <Nav.Link as={Link} to="/Contact">
+                  Contact
+                </Nav.Link>
+                <Nav.Link as={Link} to="/About">
+                  About
+                </Nav.Link>
+              </Nav>
+            </Offcanvas.Body>
+          </Navbar.Offcanvas>
+        </Container>
+      </Navbar>
+    </>
   );
 }
 
