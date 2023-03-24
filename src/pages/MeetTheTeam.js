016bcb07caae34891558144e5db8a545f1906b2f
Hasithjay98 <eshan.20210150@iit.ac.lk>
2023-03-24 18:30:30 +0530

    added linked in button to meet the team page

--- src/pages/MeetTheTeam.js
+++ src/pages/MeetTheTeam.js
@@ -2,76 +2,109 @@ import React, { Component } from "react";
 import { Container } from "react-bootstrap";
 import Card from "react-bootstrap/Card";
 import CardGroup from "react-bootstrap/CardGroup";
-import imgDumidu from "../images/TeamMembersPhotos/dumindu.jpg"
-import imgTharusha from "../images/TeamMembersPhotos/tharusha.jpg"
-import imgPasindu from "../images/TeamMembersPhotos/pasindu.jpg"
-import imgEshan from "../images/TeamMembersPhotos/eshan.jpg"
-import imgSanuri from "../images/TeamMembersPhotos/sanuri.jpg"
+import imgDumidu from "../images/TeamMembersPhotos/dumindu.jpg";
+import imgTharusha from "../images/TeamMembersPhotos/tharusha.jpg";
+import imgPasindu from "../images/TeamMembersPhotos/pasindu.jpg";
+import imgEshan from "../images/TeamMembersPhotos/eshan.jpg";
+import imgSanuri from "../images/TeamMembersPhotos/sanuri.jpg";
+import { FaLinkedin } from "react-icons/fa";
+import Button from "react-bootstrap/Button";
 
 function MeetTheTeam() {
   return (
     <>
       <div className="meettheteammain">
-        <h1 className="page-name" style={{color: "black", fontWeight: "bold"}} >
-          <center >Meet The Team</center>
+        <h1
+          className="page-name"
+          style={{ color: "black", fontWeight: "bold" }}
+        >
+          <center>Meet The Team</center>
         </h1>
         <CardGroup className="fade-in">
           <Card>
-            <Card.Img
-              variant="top"
-              src={imgDumidu}
-            />
+            <Card.Img variant="top" src={imgDumidu} />
             <Card.Body>
               <Card.Title>Dumindu Sameendra</Card.Title>
               <Card.Text>Team Leader</Card.Text>
+              <Button
+                href="https://www.linkedin.com/in/dumindu-sameendra/"
+                target="_blank"
+                rel="noopener noreferrer"
+                variant="light"
+                className="linkedin-button"
+                style={{ marginTop: "10px" }}
+              >
+                <FaLinkedin size={24} />
+              </Button>
+              
             </Card.Body>
           </Card>
           <Card>
-            <Card.Img
-              variant="top"
-              src={imgTharusha}
-            />
+            <Card.Img variant="top" src={imgTharusha} />
             <Card.Body>
               <Card.Title>Tharusha Dilshan</Card.Title>
-              <Card.Text>
-                Developer
-              </Card.Text>
+              <Card.Text>Developer</Card.Text>
+              <Button
+                href="https://www.linkedin.com/in/dumindu-sameendra/"
+                target="_blank"
+                rel="noopener noreferrer"
+                variant="light"
+                className="linkedin-button"
+                style={{ marginTop: "10px" }}
+              >
+                <FaLinkedin size={24} />
+              </Button>
             </Card.Body>
           </Card>
           <Card>
-            <Card.Img
-              variant="top"
-              src={imgPasindu}
-            />
+            <Card.Img variant="top" src={imgPasindu} />
             <Card.Body>
               <Card.Title>Pasindu Theekshana</Card.Title>
-              <Card.Text>
-              Developer
-              </Card.Text>
+              <Card.Text>Developer</Card.Text>
+              <Button
+                href="https://www.linkedin.com/in/dumindu-sameendra/"
+                target="_blank"
+                rel="noopener noreferrer"
+                variant="light"
+                className="linkedin-button"
+                style={{ marginTop: "10px" }}
+              >
+                <FaLinkedin size={24} />
+              </Button>
             </Card.Body>
           </Card>
           <Card>
-            <Card.Img
-              variant="top"
-              src={imgEshan}
-            />
+            <Card.Img variant="top" src={imgEshan} />
             <Card.Body>
               <Card.Title>Eshan Hasitha</Card.Title>
-              <Card.Text>
-              Developer
-              </Card.Text>
+              <Card.Text>Developer</Card.Text>
+              <Button
+                href="https://www.linkedin.com/in/dumindu-sameendra/"
+                target="_blank"
+                rel="noopener noreferrer"
+                variant="light"
+                className="linkedin-button"
+                style={{ marginTop: "10px" }}
+              >
+                <FaLinkedin size={24} />
+              </Button>
             </Card.Body>
           </Card>
           <Card>
-            <Card.Img
-              variant="top"
-              src={imgSanuri}
-            />
+            <Card.Img variant="top" src={imgSanuri} />
             <Card.Body>
               <Card.Title>Sanuri Perera</Card.Title>
-              <Card.Text>
-              Developer
-              </Card.Text>
+              <Card.Text>Developer</Card.Text>
+              <Button
+                href="https://www.linkedin.com/in/dumindu-sameendra/"
+                target="_blank"
+                rel="noopener noreferrer"
+                variant="light"
+                className="linkedin-button"
+                style={{ marginTop: "10px" }}
+              >
+                <FaLinkedin size={24} />
+              </Button>
             </Card.Body>
           </Card>
         </CardGroup>
