064f28eaa3bb2530de89ee6046fb83f64002e366
dsplayerX <dumindu0043@gmail.com>
2023-03-25 16:34:54 +0530

    changed home welcome text and added to sentitube logic

--- src/pages/SentiTubeLogic.js
+++ src/pages/SentiTubeLogic.js
@@ -4,20 +4,21 @@ import SentiLogicImg from "../images/sentitubelogictrans.png";
 function SentiTubeLogic() {
   return (
     <div>
-      <Container 
-      style={{
-        color: "black",
-        backdropFilter: "blur(20px)",
-        background: "rgba(255, 255, 255, 0.75)",
-        boxShadow: "0 0 10px rgba(0, 0, 0, 0.5)",
-        // marginLeft: "5%",
-        // marginTop: "2.5%",
-        // marginBottom: "2.5%",
-        // marginRight: "5%",
-        borderRadius: "20px",
-        padding: "2.5%",
-        marginBottom:"10px"
-        }}>
+      <Container
+        style={{
+          color: "black",
+          backdropFilter: "blur(20px)",
+          background: "rgba(255, 255, 255, 0.75)",
+          boxShadow: "0 0 10px rgba(0, 0, 0, 0.5)",
+          // marginLeft: "5%",
+          // marginTop: "2.5%",
+          // marginBottom: "2.5%",
+          // marginRight: "5%",
+          borderRadius: "20px",
+          padding: "2.5%",
+          marginBottom: "10px",
+        }}
+      >
         <Col>
           <Row>
             <center>
@@ -58,9 +59,13 @@ function SentiTubeLogic() {
             <h5>Positive Statements</h5>
             <p>
               A statement that expresses positive sentiment without sarcasm
-              could be classified as clearly positive. A statement that
-              expresses positive sentiment but with sarcasm could be classified
-              as negative, depending on the degree of sarcasm.
+              could be classified as clearly positive. However, in SentiTube's
+              logic, a statement that is marked as positive by sentiment
+              analysis but also identified as sarcastic by sarcasm analysis
+              would be classified as negative. This is because SentiTube
+              considers sarcasm to be a form of negation, and thus, sarcastic
+              comments that express positive sentiment are not considered to be
+              genuinely positive.
             </p>
           </Row>
           <Row>
@@ -81,9 +86,11 @@ function SentiTubeLogic() {
               A statement that is neutral in sentiment and not sarcastic should
               be classified as neutral. A statement that is neutral in sentiment
               but sarcastic could be classified as negative or neutral,
-              depending on the degree of sarcasm. (This is not yet taken into
-              account in SentiTube and will be implemented with more research
-              and more accurate models down the road.)
+              depending on the degree of sarcasm. However, in SentiTube's logic,
+              a statement that is marked as neutral by sentiment analysis would
+              be considered as still neutral in sentiment as we currently do not
+              have a way to measure the degree of sarcasm. With more research,
+              this might also be taken into account for Sentitube in the future.
             </p>
           </Row>
           <Row>
@@ -111,12 +118,18 @@ function SentiTubeLogic() {
               >
                 News Headline Sarcasm Detection by Gabriel Atkin
               </a>{" "}
-              <span style={{ fontSize: "smaller" }}>
-                (Note: Sarcastic comments on YouTube can sometimes differ very
-                much from sarcasm found on news headlines. We are planning to
-                add more accurate and in-context model for sarcasm detection in
-                the near future.)
+              <span style={{ fontSize: "smaller", color: "grey" }}>
+                (Please note that identifying sarcasm is a tricky process and
+                the accuracy of our sarcsam detection model is 78%. Sarcastic
+                comments on YouTube can sometimes differ very much from sarcasm
+                found on news headlines. This might sometimes result in wrongly
+                identified sarcasm in comments, thus showing more negativity
+                than reality. We are planning to add more accurate and
+                in-context model for sarcasm detection in the future.)
               </span>
+            </p>
+            <p>
+              <span style={{ fontSize: "smaller", color: "grey" }}></span>
             </p>
           </Row>
         </Col>
