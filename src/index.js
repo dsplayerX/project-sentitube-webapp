22d01d4d85e55d49df347d304a1ca6428566822f
dsplayerX <dumindu0043@gmail.com>
2023-03-25 15:55:47 +0530

    Update index.js

--- src/index.js
+++ src/index.js
@@ -2,7 +2,6 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./App";
-import "./Navbar_style.css";
 import { HashRouter } from "react-router-dom";
 import "bootstrap/dist/css/bootstrap.min.css";
 import ScrollToTop from "./components/ScrollToTop";
